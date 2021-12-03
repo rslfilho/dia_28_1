323c6bf171f137c78d757572396e4d76268cde64
Roberval Filho <rslfilho@gmail.com>
2021-12-03 16:41:21 -0300

    Usando backend

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState('');
+  useEffect(() => {
+    (async () => {
+      const response = await fetch('https://rslfilho-dia-28-1-back.herokuapp.com/');
+      setText(response.data.text);
+    })()
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          {
+            text
+          }
         </p>
         <a
           className="App-link"
